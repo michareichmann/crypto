3ed72655dcc2ea3ea5a6049d75a3b2af4dc223f7
michareichmann <micha.reichmann@gmail.com>
2023-08-16 21:21:25 +0200

    add file reading and saving

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-
+from src.data import *
 
 
 if __name__ == '__main__':
-    ...
+    d = Data()
+    f = d.convert()
+
 
--- src/data.py
+++ src/data.py
@@ -1,19 +1,28 @@
 from h5 import File
+from plotting.utils import BaseDir
+import numpy as np
+import pandas as pd
+import pyarrow.feather as feather
+
 
 
 class Data:
 
-    FileName = 'data.h5'
+    Dir = BaseDir.joinpath('data')
+    FilePath = Dir.joinpath('data.feather')
 
     def __init__(self):
-        self.File
+        self.File: feather = self.load()
 
     def check(self):
         """check if there is new data"""
+        return list(Data.Dir.glob('*.csv'))
 
-
-    def load(self):
+    def load(self) -> feather:
         """load the data from the h5 file"""
 
     def convert(self):
         """convert the data from the csv files to h5"""
+        fname = self.check()[0]
+        df = pd.read_csv(fname, header=0, parse_dates=[2, 3])
+        feather.write_feather(df, Data.FilePath)
