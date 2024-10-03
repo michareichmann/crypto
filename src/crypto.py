b8cf4e85f8fc0d76a990d1f6d354ba864475f392
michareichmann <micha.reichmann@gmail.com>
2024-10-03 21:22:30 +0200

    encrypt alpaca key and capitalise static fields

--- src/crypto.py
+++ src/crypto.py
@@ -4,12 +4,13 @@ from alpaca.data.live import CryptoDataStream
 from alpaca.data.requests import CryptoLatestQuoteRequest
 from cryptography.fernet import Fernet
 
-from plotting.save import prep_kw, Config, BaseDir, SaveDraw
-from src.data import Data, pd
-
+from plotting.save import prep_kw, SaveDraw
+from src.data import Data, pd, Path
 
 draw = SaveDraw()
-API_FILE = Data.Dir.joinpath('api.key')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+CONF_DIR = BASE_DIR.joinpath('config')
 
 
 async def quote_data_handler(data):
@@ -25,39 +26,38 @@ def create_key(force=False):
         return key_file.read()
 
 
-def encrypt_pw(pw: str):
-    if not API_FILE.exists():
+def encrypt_pw(pw: str, f: Path):
+    if not f.exists():
         cipher_suite = Fernet(create_key())
-        with open(API_FILE, 'wb') as key_file:
+        with open(f, 'wb') as key_file:
             key_file.write(cipher_suite.encrypt(pw.encode('utf-8')))
 
 
-def read_pw():
-    with open(API_FILE, 'rb') as f:
+def read_pw(f: Path):
+    with open(f, 'rb') as f:
         cipher_suite = Fernet(create_key())
         return cipher_suite.decrypt(f.read()).decode()
 
 
-def get_rate(base_cur='CHF', dest_cur='USD'):
-    url = f'https://v6.exchangerate-api.com/v6/{read_pw()}/latest/{base_cur}'
-    data = requests.get(url).json()
-    return data['conversion_rates'][dest_cur]
-
-
 class Alpaca:
 
-    Config = Config(BaseDir.joinpath('config', 'alpaca.ini'), section='main')
-    Symbols = ['ETH/USD', 'DOT/USD']
-    Data = CryptoHistoricalDataClient(Config.get_value('k'), Config.get_value('s'))
-    R = CryptoLatestQuoteRequest(symbol_or_symbols=Symbols)
+    EX_RATE_API_FILE = CONF_DIR.joinpath('ex_rate_api.key')
+    SECRET_FILE = CONF_DIR.joinpath('alpaca.key')
+    KEY = 'AK713DPS3TVT9N0ZMUXZ'
+
+    SYMBOLS = ['ETH/USD', 'DOT/USD']
+    DATA = CryptoHistoricalDataClient(KEY, read_pw(SECRET_FILE))
+    R = CryptoLatestQuoteRequest(symbol_or_symbols=SYMBOLS)
 
     @property
-    def exchange_rate(self):
-        return get_rate('USD', 'CHF')
+    def exchange_rate(self, base_cur='CHF', dest_cur='USD'):
+        url = f'https://v6.exchangerate-api.com/v6/{read_pw(Alpaca.EX_RATE_API_FILE)}/latest/{base_cur}'
+        data = requests.get(url).json()
+        return data['conversion_rates'][dest_cur]
 
     @property
     def data(self):
-        return self.Data.get_crypto_latest_quote(self.R)
+        return self.DATA.get_crypto_latest_quote(self.R)
 
     def ask_price(self, currency='ETH'):
         return self.data[f'{currency}/USD'].ask_price * self.exchange_rate
@@ -65,9 +65,10 @@ class Alpaca:
     def bid_price(self, currency='ETH'):
         return self.data[f'{currency}/USD'].bid_price * self.exchange_rate
 
-    def prep_stream(self):
-        s = CryptoDataStream(self.Config.get_value('k'), self.Config.get_value('s'))
-        s.subscribe_quotes(quote_data_handler, *Alpaca.Symbols)
+    @staticmethod
+    def prep_stream():
+        s = CryptoDataStream(Alpaca.KEY, read_pw(Alpaca.SECRET_FILE))
+        s.subscribe_quotes(quote_data_handler, *Alpaca.SYMBOLS)
         return s
 
 
@@ -76,10 +77,10 @@ alpaca = Alpaca()
 
 class Crypto(Data):
 
-    Name = None
+    NAME = None
 
     def __init__(self, name):
-        self.Name = name
+        self.NAME = name
         super().__init__()
 
     # def __getitem__(self, item):
@@ -87,11 +88,11 @@ class Crypto(Data):
 
     def load(self) -> pd.DataFrame:
         d = super().load()
-        return d[d[Data.SYMBOL_KEY] == self.Name].drop(columns=Data.SYMBOL_KEY).reset_index(drop=True)
+        return d[d[Data.SYMBOL_KEY] == self.NAME].drop(columns=Data.SYMBOL_KEY).reset_index(drop=True)
 
     @property
     def rate(self):
-        return alpaca.ask_price(self.Name)
+        return alpaca.ask_price(self.NAME)
 
     # region Types
     @property
@@ -104,7 +105,7 @@ class Crypto(Data):
 
     @property
     def transfers(self) -> pd.DataFrame:
-        return self[self.Description == f'Staking for currency {self.Name}']
+        return self[self.Description == f'Staking for currency {self.NAME}']
 
     @property
     def balance_(self) -> pd.DataFrame:
@@ -124,5 +125,5 @@ class Crypto(Data):
         x, y = self.time(d), self.balance(d)
         if week or month:
             b = self.week_bins(d) if week else self.month_bins(d) if month else None
-            return draw.profile(x, y, b, **prep_kw(dkw, **self.x_args(week, month, 1), graph=True, y_tit=f'Balance [{self.Name}]'))
-        return draw.graph(x, y, **prep_kw(dkw, **self.x_args(), markersize=.7, y_tit=f'Balance [{self.Name}]'))
+            return draw.profile(x, y, b, **prep_kw(dkw, **self.x_args(week, month, 1), graph=True, y_tit=f'Balance [{self.NAME}]'))
+        return draw.graph(x, y, **prep_kw(dkw, **self.x_args(), markersize=.7, y_tit=f'Balance [{self.NAME}]'))
